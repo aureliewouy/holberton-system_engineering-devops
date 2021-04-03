f42988a6af203589435a4e65aac614261d29f726
aureliewouy <aureliecedia@yahoo.com>
2021-04-03 13:26:36 +0000

    pep-lint compliance

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
 # Set up your client SSH configuration file
 
 file_line { 'Turn off passwd auth':
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no'
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no'
 }
 file_line { 'Declare identity file':
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/holberton'
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/holberton'
 }
