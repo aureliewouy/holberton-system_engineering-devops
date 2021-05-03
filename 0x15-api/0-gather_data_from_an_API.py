c024c2acd9cd4f64706226eb374b70a0a64eccc6
aureliewouy <aureliecedia@yahoo.com>
2021-05-03 10:24:05 +0000

    update

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Python script that, using this REST API """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """code should not be executed when imported"""
+    if len(argv) == 2:
+        usr = argv[1]
+        if usr.isdigit():
+            api_user = "https://jsonplaceholder.typicode.com/users/{}"
+            .format(usr)
+            resp = requests.get(api_user)
+            usr_name = resp.json().get("name")
+            api_todo = 'https://jsonplaceholder.typicode.com/todos/?userId={}'
+            .format(usr)
+            todos = requests.get(api_todo)
+            todos_title = todos.json()
+            total = 0
+            done = 0
+            usr_todo = []
+            for todo in todos_title:
+                total += 1
+                completed = todo.get('completed')
+                if completed is True:
+                    usr_todo.append(todo['title'])
+                    done += 1
+            print("Employee {} is done with tasks({}/{}):"
+                  .format(usr_name, done, total))
+            for to in usr_todo:
+                print("\t {}".format(to))
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Python script to export data in the CSV format """
+import requests
+from sys import argv
+import csv
+
+
+if __name__ == "__main__":
+    """code should not be executed when imported"""
+    if len(argv) == 2:
+        usr = argv[1]
+        if usr.isdigit():
+            api_user = "https://jsonplaceholder.typicode.com/users/{}"
+            .format(usr)
+            resp = requests.get(api_user)
+            usr_name = resp.json().get("name")
+            api_todo = 'https://jsonplaceholder.typicode.com/todos/?userId={}'
+            .format(usr)
+            todos = requests.get(api_todo)
+            todos_title = todos.json()
+            total = 0
+            done = 0
+            usr_todo = []
+            for todo in todos_title:
+                total += 1
+                completed = todo.get('completed')
+                if completed is True:
+                    usr_todo.append(todo['title'])
+                    done += 1
+
+                with open(usr + '.csv', mode='a+') as csv_file:
+                    wr = csv.writer(csv_file, delimiter=',',
+                                    quoting=csv.QUOTE_ALL)
+                    wr.writerow([usr, usr_name, completed, todo['title']])
