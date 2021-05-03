040ea77d96b2d6755ec8d46e05f32471c11cf101
aureliewouy <aureliecedia@yahoo.com>
2021-05-03 17:02:12 +0000

    dump

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -24,4 +24,4 @@ if __name__ == "__main__":
             usr_todo.append(todo_dict)
         new[usr_id] = usr_todo
     with open('todo_all_employees.json', mode='w') as f:
-        f.write(json.dumps(new))
+        json.dumps(new, f)
