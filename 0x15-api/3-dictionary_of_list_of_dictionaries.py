fbb8a7a67a22943ffe9cc88d7ddbf89db3544b8b
aureliewouy <aureliecedia@yahoo.com>
2021-05-03 12:51:37 +0000

    remove useless lines

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -11,17 +11,15 @@ if __name__ == "__main__":
     users = requests.get(api_user).json()
     new = {}
     for user in users:
-        usr_name = user.get("name")
         usr_id = user.get("id")
         aptodo = 'https://jsonplaceholder.typicode.com/todos/?userId={}'\
             .format(usr_id)
-        todos = requests.get(aptodo)
-        todos_title = todos.json()
+        todos = requests.get(aptodo).json()
         usr_todo = []
-        for todo in todos_title:
-            todo_dict = {"username": usr_name,
-                         "task": todo['title'],
-                         "completed": todo['completed']}
+        for todo in todos:
+            todo_dict = {"username": user.get("name"),
+                         "task": todo.get('title'),
+                         "completed": todo.get('completed')}
             usr_todo.append(todo_dict)
         new[usr_id] = usr_todo
     with open('todo_all_employees.json', mode='a+') as f:
